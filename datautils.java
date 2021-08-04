a1897e0dcc6e5f584cb6c449e3d1c780316b2b87
phanijalaparthiPJ <phanichowdary1912@gmail.com>
2021-08-04 10:37:20 +0530

    exiting

--- datautils.java
+++ datautils.java
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
+=======
+im working on stash
+im working with dev branch
+>>>>>>> 933b8a9 (only existing file)
 im working with master branch
 Hi guys started work today
---
+++ ora.sh
@@ -0,0 +1,3 @@
+hi all we discuss ora
+hi this is database
+
